1c70554695409ae9b011442cd1f3f47c3d7f28d0
fademar <fdemarthon@gmail.com>
2017-07-19 18:27:28 +0200

    details what

--- src/app/components/deed-details/deed-details.component.ts
+++ src/app/components/deed-details/deed-details.component.ts
@@ -12,6 +12,12 @@ export class DeedDetailsComponent implements OnInit {
   id: string;
   deed;
   sub;
+  selectedActions = this.selectedActions;
+  selectedAction;
+  selectedCounterActions = this.selectedCounterActions;
+  selectedCounterAction;
+  
+
   term = this.term;
   constructor(private deedService:DeedService, private router: Router, private route: ActivatedRoute) { }
 
@@ -20,6 +26,101 @@ export class DeedDetailsComponent implements OnInit {
     
     this.deedService.getDeed(this.id).subscribe(deed => {
       this.deed = deed;
+      this.selectedActions = [];
+      this.selectedCounterActions = [];
+
+      this.deed.transactions.forEach(transaction => {
+        console.log(transaction);
+        switch (transaction.agentAction) {
+
+            case 'cedes': {
+                this.selectedAction = 'what';
+                this.selectedCounterAction = 'what';
+                break;
+            }
+            case 'exchanges': {
+                this.selectedAction = 'what';
+                this.selectedCounterAction = 'what';
+                break;
+            }
+            case 'mortgages': {
+                this.selectedAction = 'what';
+                this.selectedCounterAction = 'what';
+                break;
+            }
+            case 'puts to rent': {
+                this.selectedAction = 'what';
+                this.selectedCounterAction = 'what';
+                break;
+            }
+            case 'sells': {
+                this.selectedAction = 'what';
+                this.selectedCounterAction = 'what';
+                break;
+            }
+            case 'donates':
+            case 'borrows': {
+                this.selectedAction = 'what';
+                this.selectedCounterAction = '';
+                break;
+            }
+            case 'agrees to marry-off': {
+                this.selectedAction = 'whom';
+                break;
+            }
+            case 'engages': {
+                this.selectedAction = 'asWhom';
+                this.selectedCounterAction = 'what';
+                break;
+            }
+            case 'bequeaths': {
+                this.selectedAction = 'bequeaths';
+                break;
+            }
+            case 'settles': {
+                this.selectedAction = 'settles';
+                this.selectedCounterAction = 'settles';
+                
+                break;
+            }
+            case 'agrees to marry': {
+                this.selectedAction = '';
+                break;
+            }
+            case 'manumits': {
+                this.selectedAction = '';
+                this.selectedCounterAction = 'what';
+                break;
+            }
+            case 'agrees to divorce':
+            case 'promises':
+            case 'elects':
+            case 'signs receipt': {
+                this.selectedAction = '';
+                this.selectedCounterAction = '';
+                break;
+            }
+            case 'other': {
+                this.selectedAction = 'other';         
+                break;
+            }
+            default: {
+                this.selectedAction = '';
+                this.selectedCounterAction = '';
+                break;
+            }
+        }
+      
+      
+        this.selectedActions.push(this.selectedAction);
+        this.selectedCounterActions.push(this.selectedCounterAction);  
+        console.log(this.selectedActions);
+        console.log(this.selectedCounterActions);
+      });
+
+      
+
+
     });
 
     this.sub = this.route
