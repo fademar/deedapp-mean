b4abb68dc4702b2953daef4b18a9470cb4fe6d61
fademar <fdemarthon@gmail.com>
2017-09-11 14:47:22 +0200

    dist

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ app.use(function (req, res, next) {
 });
 
 // Create link to Angular build directory
-// const distDir = __dirname + "/dist/";
-// app.use(express.static(distDir));
+const distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 // Create a db const to reuse the connection
 var db;
