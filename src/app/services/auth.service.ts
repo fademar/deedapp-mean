58e359825e2b17506a69a2a6f53d7b9b0e90f9ed
fademar <fdemarthon@gmail.com>
2017-09-12 13:06:26 +0200

    callback

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -13,7 +13,7 @@ export class AuthService {
     domain: 'cercec.eu.auth0.com',
     responseType: 'token id_token',
     audience: 'https://cercec.eu.auth0.com/userinfo',
-    redirectUri: 'http://194.57.136.170/callback',      
+    redirectUri: 'https://russian-deeds.herokuapp.com/callback',      
     scope: 'openid'
   });
 
