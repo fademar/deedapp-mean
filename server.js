17283f1088fa5e98ba5fbbf09af32ae1e82e4706
fademar <fdemarthon@gmail.com>
2017-09-13 17:48:53 +0200

    server and index

--- server.js
+++ server.js
@@ -16,15 +16,13 @@ app.use(bodyParser.json());
 
 const dbURL = "mongodb://fadem:886682@ds061248.mlab.com:61248/dbdeeds"
 
-
-
 // // Enable CORS 
-// app.use(function (req, res, next) {
-// 	res.header('Access-Control-Allow-Origin', '*');
-// 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-// 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-// 	next();
-// });
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
 
 // Create link to Angular build directory
 const distDir = __dirname + "/dist/";
