7bea02c1f33b3a1a4d91a4bb796ed99b215de004
fademar <fdemarthon@gmail.com>
2017-09-21 16:33:41 +0200

    server

--- server.js
+++ server.js
@@ -22,13 +22,9 @@ app.use(function (req, res, next) {
 	next();
 });
 
-// Create link to Angular build directory
-const distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
 
 // Create a db const to reuse the connection
 var db;
@@ -180,4 +176,8 @@ app.get('/api/search/:term', (req, res) => {
 			res.status(200).json(docs);
 		}
 	});
+});
+
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
