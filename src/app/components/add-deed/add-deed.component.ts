41c38f1c20ee3f932c03b2d96ded681659a5a2be
fademar <fdemarthon@gmail.com>
2017-10-12 15:31:24 +0200

    radio

--- src/app/components/add-deed/add-deed.component.ts
+++ src/app/components/add-deed/add-deed.component.ts
@@ -173,7 +173,7 @@ export class AddDeedComponent implements OnInit {
             }),
             verbatimCitations: [''],
             researcherNotes: [''],
-            complete: [false]
+            complete: ['false']
         })
 
     }
