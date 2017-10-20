ee1a81393849997d90935998e4eec84b3f098353
fademar <fdemarthon@gmail.com>
2017-10-20 16:31:14 +0200

    date

--- src/app/components/deeds/deeds.component.ts
+++ src/app/components/deeds/deeds.component.ts
@@ -55,7 +55,7 @@ export class DeedsComponent implements OnInit {
 			let json = JSON.stringify(this.deeds);
 			this.downloadUri = this.sanitizer.bypassSecurityTrustUrl("data:text/json;charset=UTF-8," + encodeURIComponent(json));
 			let date = new Date;
-			this.downloadName = date.getDate() + '_' + date.getTime() + '.json';
+			this.downloadName = date.getDate() + '-' + date.getMonth() + '-' + date.getFullYear() + '_' + date.getHours() + '-' + date.getMinutes() + '-' + date.getSeconds() + '.json';
 		
 		
 		
