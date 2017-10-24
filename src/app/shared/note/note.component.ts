b38f159f0cadebe14ecedea505201102ee885cd9
fademar <fdemarthon@gmail.com>
2017-10-24 13:39:00 +0200

    dialog

--- src/app/shared/note/note.component.ts
+++ src/app/shared/note/note.component.ts
@@ -20,6 +20,7 @@ export class NoteComponent implements OnInit {
   openNotes(): void {
     let dialogRef = this.dialog.open(NoteDialog, {
       width: '80%',
+      height: '80%',
       disableClose: false
     });
 
