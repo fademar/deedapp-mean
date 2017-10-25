3b8eb524a5ab11d963bc7707db3430bc797d3b21
fademar <fdemarthon@gmail.com>
2017-10-25 20:09:59 +0200

    false

--- src/app/components/add-deed/add-deed.component.ts
+++ src/app/components/add-deed/add-deed.component.ts
@@ -178,7 +178,7 @@ export class AddDeedComponent implements OnInit {
             }),
             verbatimCitations: [''],
             researcherNotes: [''],
-            complete: ['false']
+            complete: [false]
         })
 
     }
