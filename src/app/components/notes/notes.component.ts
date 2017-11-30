e80ac0562c011847c76880e861b5cdfb6df3ef8d
fademar <fdemarthon@gmail.com>
2017-11-30 18:53:04 +0100

    notes

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormArray, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { NoteService } from '../../services/note.service';
+import { DatePipe } from '@angular/common';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-notes',
@@ -7,9 +12,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesComponent implements OnInit {
 
-  constructor() { }
+  noteForm: FormGroup;
+  user: FormControl;
+  date: FormGroup;
+  content: FormControl;
+  noteValue;
+  note;
+  notes;
+  contentClass = this.contentClass;
+  editMode = false;
+  noteId = null;
+
+  constructor(private noteService: NoteService, private fb: FormBuilder, public auth: AuthService) { }
 
   ngOnInit() {
+    this.initForm();
+    this.showNotes();
   }
+
+  initForm() {
+    this.noteForm = this.fb.group({
+      user: [''],
+      date: [''],
+      content: ['']
+    })
+  }
+
+  showNotes() {
+    this.noteService.getNotes().subscribe(notes => {
+      this.notes = notes;
+    });
+  }
+
+
+  getUser() {
+    return localStorage.getItem('userName');
+  }
+
+  onSubmit() {
+    let time = Date.now()
+    let userName = this.getUser();
+		this.noteForm.patchValue({
+      date: time, 
+      user: userName
+    });
+    this.noteValue = JSON.stringify(this.noteForm.value);
+    if (this.editMode) {
+      this.noteService.updateNote(this.noteId, this.noteValue).subscribe(note => {
+        this.showNotes();
+        this.noteId = null;
+        this.editMode = false;
+      });
+    } else {
+      this.noteService.saveNote(this.noteValue).subscribe(note => {
+        this.showNotes();
+      });
+    }
+    this.noteForm.reset();
+  }
+
+
+  onEditClick(id) {
+    this.editMode = true;
+    this.noteId = id;
+    this.noteService.getNote(id).subscribe(note => {
+      this.note = note;
+      this.noteForm.patchValue({
+        content: this.note.content
+      });
+    });
+  }
+
+  onDeleteClick(id) {
+    this.noteService.deleteNote(id).subscribe(note => {
+      this.showNotes();
+    });
+  }
+
 
 }
