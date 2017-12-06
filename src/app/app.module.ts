2aefd5dbed7f4b78102127f03fadd8c579611a78
fademar <fdemarthon@gmail.com>
2017-12-06 17:19:44 +0100

    notes side nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MatInputModule } from '@angular/material';
 import { MatRadioModule } from '@angular/material';
 import { MatButtonToggleModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -96,7 +97,8 @@ import { NewWindowDirective } from './directives/new-window.directive';
     MatInputModule,
     MatRadioModule,
     MatButtonToggleModule,
-    MatToolbarModule 
+    MatToolbarModule, 
+    MatSidenavModule 
   ],
   entryComponents: [ConfirmDialogComponent, NoteDialog],
   providers: [Title, DeedService, SchemaService, SearchService, PagerService, AuthService, AuthGuard, DownloadService, NoteService],
