bc60d60435192978dba9a336fb56916282f18433
fademar <fdemarthon@gmail.com>
2017-12-22 08:43:42 +0100

    pipe strip tags

--- src/app/pipes/excerpt.pipe.ts
+++ src/app/pipes/excerpt.pipe.ts
@@ -7,9 +7,10 @@ export class ExcerptPipe implements PipeTransform {
 
   transform(text: any, length: number): any {
     if (!text || !length) {
-      return text;
+      return text.replace(/<\/?[^>]+>/gi, '');
     }
     if (text.length > length) {
+      text.replace(/<\/?[^>]+>/gi, '')
       return text.substr(0, length) + '...';
     }
     
