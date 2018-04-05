18806b04ef751ee5271376cde1040deed12a8f3f
fademar <fdemarthon@gmail.com>
2018-04-05 17:46:25 +0200

    typeahead

--- src/app/components/add-deed/add-deed.component.ts
+++ src/app/components/add-deed/add-deed.component.ts
@@ -145,12 +145,38 @@ export class AddDeedComponent implements OnInit {
 
         // Create Typeahead arrays for firstnames
         this.deedService.getDeeds().subscribe(deeds => {
-            deeds.forEach(element => {
-                if (element.agentSex === 'male' && element.agent.firstName) {
-                    this.firstNamesMale.push(element.agent.firstName);
+            deeds.forEach(deed => {
+                if (deed.agentSex === 'male' && deed.agent.firstName) {
+                    this.firstNamesMale.push(deed.agent.firstName);
                 }
-                if (element.agentSex === 'female' && element.agent.firstName) {
-                    this.firstNamesFemale.push(element.agent.firstName);
+                if (deed.agentSex === 'female' && deed.agent.firstName) {
+                    this.firstNamesFemale.push(deed.agent.firstName);
+                }
+                if (deed.counterAgentSex === 'male' && deed.counterAgent.firstName) {
+                    this.firstNamesMale.push(deed.counterAgent.firstName);
+                }
+                if (deed.counterAgentSex === 'female' && deed.counterAgent.firstName) {
+                    this.firstNamesFemale.push(deed.counterAgent.firstName);
+                }
+                if (deed.coAgents.length > 0) {
+                    deed.coAgents.forEach(element => {
+                        if (element.coAgentSex === 'male' && element.coAgent.firstName) {
+                            this.firstNamesMale.push(element.coAgent.firstName);
+                        }
+                        if (element.coAgentSex === 'female' && element.coAgent.firstName) {
+                            this.firstNamesFemale.push(element.coAgent.firstName);
+                        }       
+                    });
+                }
+                if (deed.coCounterAgents.length > 0) {
+                    deed.coCounterAgents.forEach(element => {
+                        if (element.coCounterAgentSex === 'male' && element.coCounterAgent.firstName) {
+                            this.firstNamesMale.push(element.coCounterAgent.firstName);
+                        }
+                        if (element.coCounterAgentSex === 'female' && element.coCounterAgent.firstName) {
+                            this.firstNamesFemale.push(element.coCounterAgent.firstName);
+                        }       
+                    });
                 }
             });
             this.firstNamesMale.sort();
