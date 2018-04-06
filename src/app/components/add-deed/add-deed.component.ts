9f95909c927a5f63edec4f8d28b67c8e07181508
fademar <fdemarthon@gmail.com>
2018-04-06 10:03:52 +0200

    sort

--- src/app/components/add-deed/add-deed.component.ts
+++ src/app/components/add-deed/add-deed.component.ts
@@ -2591,69 +2591,69 @@ export class AddDeedComponent implements OnInit {
 
     getFirstNames() {
         this.deedService.getDeeds().subscribe(deeds => {
-          deeds.forEach(deed => {
-            if (deed.agentSex === 'male' && deed.agent.firstName) {
-              this.firstNamesMale.push(deed.agent.firstName);
-            }
-            if (deed.agentSex === 'female' && deed.agent.referentMale.firstName) {
-              this.firstNamesMale.push(deed.agent.referentMale.firstName);
-            }
-            if (deed.counterAgentSex === 'male' && deed.counterAgent.firstName) {
-              this.firstNamesMale.push(deed.counterAgent.firstName);
-            }
-            if (deed.counterAgentSex === 'female' && deed.counterAgent.referentMale.firstName) {
-              this.firstNamesFemale.push(deed.counterAgent.referentMale.firstName);
-            }
-            if (deed.coAgents.length > 0) {
-              deed.coAgents.forEach(element => {
-                if (element.coAgentSex === 'male' && element.coAgent.firstName) {
-                  this.firstNamesMale.push(element.coAgent.firstName);
+            deeds.forEach(deed => {
+                if (deed.agentSex === 'male' && deed.agent.firstName) {
+                    this.firstNamesMale.push(deed.agent.firstName);
                 }
-                if (element.coAgentSex === 'female' && element.coAgent.referentMale.firstName) {
-                  this.firstNamesMale.push(element.coAgent.referentMale.firstName);
+                if (deed.agentSex === 'female' && deed.agent.referentMale.firstName) {
+                    this.firstNamesMale.push(deed.agent.referentMale.firstName);
                 }
-              });
-            }
-            if (deed.coCounterAgents.length > 0) {
-              deed.coCounterAgents.forEach(element => {
-                if (element.coCounterAgentSex === 'male' && element.coCounterAgent.firstName) {
-                  this.firstNamesMale.push(element.coCounterAgent.firstName);
+                if (deed.counterAgentSex === 'male' && deed.counterAgent.firstName) {
+                    this.firstNamesMale.push(deed.counterAgent.firstName);
                 }
-                if (element.coCounterAgentSex === 'female' && element.coCounterAgent.referentMale.firstName) {
-                  this.firstNamesMale.push(element.coCounterAgent.referentMale.firstName);
+                if (deed.counterAgentSex === 'female' && deed.counterAgent.referentMale.firstName) {
+                    this.firstNamesFemale.push(deed.counterAgent.referentMale.firstName);
                 }
-              });
-            }
-            if (deed.agentSex === 'female' && deed.agent.firstName) {
-                this.firstNamesFemale.push(deed.agent.firstName);
-              }
-      
-              if (deed.counterAgentSex === 'female' && deed.counterAgent.firstName) {
-                this.firstNamesFemale.push(deed.counterAgent.firstName);
-              }
-              if (deed.coAgents.length > 0) {
-                deed.coAgents.forEach(element => {
-                  if (element.coAgentSex === 'female' && element.coAgent.firstName) {
-                    this.firstNamesFemale.push(element.coAgent.firstName);
-                  }
-                });
-              }
-              if (deed.coCounterAgents.length > 0) {
-                deed.coCounterAgents.forEach(element => {
-                  if (element.coCounterAgentSex === 'female' && element.coCounterAgent.firstName) {
-                    this.firstNamesFemale.push(element.coCounterAgent.firstName);
-                  }
-                });
-              }
-          });
+                if (deed.coAgents.length > 0) {
+                    deed.coAgents.forEach(element => {
+                        if (element.coAgentSex === 'male' && element.coAgent.firstName) {
+                            this.firstNamesMale.push(element.coAgent.firstName);
+                        }
+                        if (element.coAgentSex === 'female' && element.coAgent.referentMale.firstName) {
+                            this.firstNamesMale.push(element.coAgent.referentMale.firstName);
+                        }
+                    });
+                }
+                if (deed.coCounterAgents.length > 0) {
+                    deed.coCounterAgents.forEach(element => {
+                        if (element.coCounterAgentSex === 'male' && element.coCounterAgent.firstName) {
+                            this.firstNamesMale.push(element.coCounterAgent.firstName);
+                        }
+                        if (element.coCounterAgentSex === 'female' && element.coCounterAgent.referentMale.firstName) {
+                            this.firstNamesMale.push(element.coCounterAgent.referentMale.firstName);
+                        }
+                    });
+                }
+                if (deed.agentSex === 'female' && deed.agent.firstName) {
+                    this.firstNamesFemale.push(deed.agent.firstName);
+                }
+
+                if (deed.counterAgentSex === 'female' && deed.counterAgent.firstName) {
+                    this.firstNamesFemale.push(deed.counterAgent.firstName);
+                }
+                if (deed.coAgents.length > 0) {
+                    deed.coAgents.forEach(element => {
+                        if (element.coAgentSex === 'female' && element.coAgent.firstName) {
+                            this.firstNamesFemale.push(element.coAgent.firstName);
+                        }
+                    });
+                }
+                if (deed.coCounterAgents.length > 0) {
+                    deed.coCounterAgents.forEach(element => {
+                        if (element.coCounterAgentSex === 'female' && element.coCounterAgent.firstName) {
+                            this.firstNamesFemale.push(element.coCounterAgent.firstName);
+                        }
+                    });
+                }
+            });
+            this.firstNamesMale.sort();
+            this.firstNamesMale = _.sortedUniq(this.firstNamesMale);
+            this.firstNamesFemale.sort();
+            this.firstNamesFemale = _.sortedUniq(this.firstNamesFemale);
         });
-        this.firstNamesMale.sort();
-        this.firstNamesMale = _.sortedUniq(this.firstNamesMale);
-        this.firstNamesFemale.sort();
-        this.firstNamesFemale = _.sortedUniq(this.firstNamesFemale);
-      }
-    
-    
+    }
+
+
 
 
     // Submit the form
