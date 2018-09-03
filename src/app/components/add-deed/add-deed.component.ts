a28ae183693c1e79687df34a50b4e3e4d089114b
fademar <fdemarthon@gmail.com>
2018-09-03 12:03:59 +0200

    amouont

--- src/app/components/add-deed/add-deed.component.ts
+++ src/app/components/add-deed/add-deed.component.ts
@@ -2142,7 +2142,7 @@ export class AddDeedComponent implements OnInit {
                                             debtDate: ['']
                                         })
                                     });
-                                    console.log(transaction.agentTransactionObjects[index].amount);
+                                    console.log(transaction.agentTransactionObjects[index]);
                                     if (transaction.agentTransactionObjects[index].debt.amount.moscowSilver || transaction.agentTransactionObjects[index].debt.amount.chekhi) {
                                         if (transaction.agentTransactionObjects[index].debt.amount.moscowSilver) {
                                             this.agentTransactionObject.controls.debt['controls'].amount['controls'][0].patchValue({
