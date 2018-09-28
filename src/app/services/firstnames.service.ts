d83df25ad9dccda62e10ef36d0a43cfd0b455e2d
fademar <fdemarthon@gmail.com>
2018-09-28 21:51:21 +0200

    local

--- src/app/services/firstnames.service.ts
+++ src/app/services/firstnames.service.ts
@@ -110,8 +110,8 @@ export class FirstnamesService {
             }
 
         }); //END FOREACH
-        firstNames.sort((a, b) => a.localeCompare(b, 'ru', {}));
-        
+        firstNames.sort((a, b) => a.firstname.localeCompare(b.firstname, 'ru', {}));
+
         return of(_.sortedUniq(firstNames));
 
     }
