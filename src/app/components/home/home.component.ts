60f65d09be3bc214895e79bc21ccd8215418725c
Fabrice Demarthon <fabricedemarthon@MacBook-de-Fabrice.local>
2018-10-23 11:29:32 +0200

    home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-import { Title }     from '@angular/platform-browser';
+import { Title } from '@angular/platform-browser';
 
 
 @Component({
