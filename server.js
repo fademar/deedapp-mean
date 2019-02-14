bda56a200f5a33525032a6959076e5d73b28e098
fademar <fdemarthon@gmail.com>
2019-02-14 16:32:31 +0100

    dictionnary

--- server.js
+++ server.js
@@ -374,16 +374,16 @@ app.post('/api/new-firstnames/', (req, res) => {
   });
 });
 
-// app.post('/api/update-firstname-dictionnary/', (req, res) => {
-//   const newNamesList = req.body;
-//   db.collection(firstNamesCollection).updateOne(
-//     {name: newNamesList.$.newName}, {$push: { versions: newNamesList.$.oldName} }, (err,res) => {
-//     if (err) {
-//       handleError(res, err.message, 'Failed to update firstname database');
-//     } else {
-//       res.status(200).json('The firstname has been updated');
-//     }
-//   });
+app.post('/api/update-firstname-dictionnary/', (req, res) => {
+  // const newNamesList = req.body;
+  // db.collection(firstNamesCollection).updateOne(
+  //   {name: newNamesList.$.newName}, {$push: { versions: newNamesList.$.oldName} }, (err,res) => {
+  //   if (err) {
+  //     handleError(res, err.message, 'Failed to update firstname database');
+  //   } else {
+  //     res.status(200).json('The firstname has been updated');
+  //   }
+  });
 
 
 });
