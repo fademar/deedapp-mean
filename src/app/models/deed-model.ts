8076c7976c534818850adcc42041eb38e5fed39d
Fabrice Demarthon <fabricedemarthon@neo116.neo.ehess.fr>
2019-09-18 13:58:31 +0200

    new models

--- src/app/models/deed-model.ts
+++ src/app/models/deed-model.ts
@@ -1,110 +1,249 @@
-
 export class Male {
-	geogrStatus: '';
-	socialStatus: '';
-	firstName: '';
-	patronyme: '';
-	lastName: '';
-	relatedTo: '';
+  geogrStatus: "";
+  socialStatus: "";
+  firstName: "";
+  patronyme: "";
+  lastName: "";
+  relatedTo: "";
 }
 
 export class Female {
-	familyStatus: '';
-	firstName: '';
-	patronyme: '';
-	relatedTo: '';
-	referentMale: {
-		relationshipToAgentSexF: '';
-		geogrStatus: '';
-		socialStatus: '';
-		firstName: '';
-		patronyme: '';
-		lastName: '';
-		relatedTo: '';
-	}
+  familyStatus: "";
+  firstName: "";
+  patronyme: "";
+  relatedTo: "";
+  referentMale: {
+    relationshipToAgentSexF: "";
+    geogrStatus: "";
+    socialStatus: "";
+    firstName: "";
+    patronyme: "";
+    lastName: "";
+    relatedTo: "";
+  };
 }
 
 export class BodyCorporate {
-	geogrStatus: '';
-	socialStatus: '';
-	corporationName: '';
-	nbParticipants: '';
-	names: '';
+  geogrStatus: "";
+  socialStatus: "";
+  corporationName: "";
+  nbParticipants: "";
+  names: "";
 }
 
 export class OtherParticipant {
-	role: '';
-	geogrStatus: '';
-	socialStatus: '';
-	firstName: '';
-	patronyme: '';
-	lastName: '';
-	relatedTo: ''
+  role: "";
+  geogrStatus: "";
+  socialStatus: "";
+  firstName: "";
+  patronyme: "";
+  lastName: "";
+  relatedTo: "";
 }
 
 export class Registrator {
-	firstName: '';
-	patronyme: '';
-	lastName: '';
-	relatedTo: ''
+  firstName: "";
+  patronyme: "";
+  lastName: "";
+  relatedTo: "";
 }
 
 export class Fee {
-	amount: '';
-	collected: ''
+  amount: "";
+  collected: "";
 }
 
 export class FirstNameObject {
-	sex: string = '';
-	firstname: string = '';
-	deedId: string = '';
-	field: string = ''
-	constructor(sex: string, firstname: string, deedId: string, field: string) {
-		this.sex = sex;
-		this.firstname = firstname;
-		this.deedId = deedId;
-		this.field = field;
-	}
-	getSex() {
-		return this.sex;
-	}
-	getFirstname() {
-		return this.firstname;
-	}
-	getId() {
-		return this.deedId;
-	}
-	getField() {
-		return this.field;
-	}
+  sex: string = "";
+  firstname: string = "";
+  deedId: string = "";
+  field: string = "";
+  constructor(sex: string, firstname: string, deedId: string, field: string) {
+    this.sex = sex;
+    this.firstname = firstname;
+    this.deedId = deedId;
+    this.field = field;
+  }
+  getSex() {
+    return this.sex;
+  }
+  getFirstname() {
+    return this.firstname;
+  }
+  getId() {
+    return this.deedId;
+  }
+  getField() {
+    return this.field;
+  }
 }
 
-export const gender = ['', 'male', 'female', 'body-corporate'];
+export const gender = ["", "male", "female", "body-corporate"];
 
-export const socialBody = ['', 'monks', 'nuns', 'parishioners', 'townsfolk', 'country folk', 'regiment', 'other']
+export const socialBody = [
+  "",
+  "monks",
+  "nuns",
+  "parishioners",
+  "townsfolk",
+  "country folk",
+  "regiment",
+  "other"
+];
 
-export const transactionTypes = ['', 'money', 'land', 'building', 'soul', 'movable', 'obligation'];
+export const transactionTypes = [
+  "",
+  "money",
+  "land",
+  "building",
+  "soul",
+  "movable",
+  "obligation"
+];
 
-export const currencies = ['', 'rouble', 'altyn', 'denga'];
+export const currencies = ["", "rouble", "altyn", "denga"];
 
-export const relationtoagents = ['', 'parents', 'corporation members', 'companions', 'other'];
+export const relationtoagents = [
+  "",
+  "parents",
+  "corporation members",
+  "companions",
+  "other"
+];
 
-export const agentActionsList = ['', 'agrees to divorce', 'agrees to marry', 'agrees to marry-off', 'bequeaths', 'borrows', 'cedes', 'donates', 'elects', 'engages', 'exchanges', 'manumits', 'mortgages', 'promises', 'puts to rent', 'sells', 'settles', 'signs receipt'];
+export const agentActionsList = [
+  "",
+  "agrees to divorce",
+  "agrees to marry",
+  "agrees to marry-off",
+  "bequeaths",
+  "borrows",
+  "cedes",
+  "donates",
+  "elects",
+  "engages",
+  "exchanges",
+  "manumits",
+  "mortgages",
+  "promises",
+  "puts to rent",
+  "sells",
+  "settles",
+  "signs receipt"
+];
 
-export const whatList = ['', 'chattels', 'debt', 'dependent', 'forfeit', 'fugitive souls', 'goods', 'immovable property', 'money', 'parent', 'responsibilities', 'share from estate', 'souls'];
+export const whatList = [
+  "",
+  "chattels",
+  "debt",
+  "dependent",
+  "forfeit",
+  "fugitive souls",
+  "goods",
+  "immovable property",
+  "money",
+  "parent",
+  "responsibilities",
+  "share from estate",
+  "souls"
+];
 
-export const immovablePropertyList = ['', 'поместье', 'вотчина', 'двор городской', 'двор сельский', 'лавочное место', 'огородное место', 'мельница', 'винница', 'пасека', 'лес', 'сенные покосы'];
+export const immovablePropertyList = [
+  "",
+  "деревня",
+  "поместье",
+  "вотчина",
+  "двор городской",
+  "двор сельский",
+  "лавочное место",
+  "огородное место",
+  "мельница",
+  "винница",
+  "пасека",
+  "лес",
+  "сенные покосы",
+  "починок",
+  "заимка"
+];
 
-export const shareList = ['', '1/2', '1/3', '1/4', 'other'];
+export const shareList = ["", "1/2", "1/3", "1/4", "other"];
 
-export const whomList = ['', 'parent', 'dependent'];
+export const whomList = ["", "parent", "dependent"];
 
-export const asWhomList = ['', 'as hired worker', 'in household', 'as son-in-law', 'as bondman', 'as peasant', 'as contractor', 'as tax-farmer', 'other'];
+export const asWhomList = [
+  "",
+  "as hired worker",
+  "in household",
+  "as son-in-law",
+  "as bondman",
+  "as peasant",
+  "as contractor",
+  "as tax-farmer",
+  "other"
+];
 
-export const activityList = ['', 'транспорт', 'строительство', 'поставка вина', 'поставка хлеба', 'other'];
+export const activityList = [
+  "",
+  "транспорт",
+  "строительство",
+  "поставка вина",
+  "поставка хлеба",
+  "other"
+];
 
-export const typeTaxList = ['', 'мельница', 'рыбная ловля', 'конская площадка', 'мост и перевоз', 'other'];
+export const typeTaxList = [
+  "",
+  "мельница",
+  "рыбная ловля",
+  "конская площадка",
+  "мост и перевоз",
+  "other"
+];
 
-export const counterAgentActionsList = ['', 'agrees to marry', 'agrees to marry-off', 'cedes', 'exchanges', 'lends', 'pays', 'settles'];
+export const counterAgentActionsList = [
+  "",
+  "agrees to marry",
+  "agrees to marry-off",
+  "cedes",
+  "exchanges",
+  "lends",
+  "pays",
+  "settles"
+];
 
-export const alphabet = ["#", "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"];
+export const alphabet = [
+  "#",
+  "А",
+  "Б",
+  "В",
+  "Г",
+  "Д",
+  "Е",
+  "Ё",
+  "Ж",
+  "З",
+  "И",
+  "Й",
+  "К",
+  "Л",
+  "М",
+  "Н",
+  "О",
+  "П",
+  "Р",
+  "С",
+  "Т",
+  "У",
+  "Ф",
+  "Х",
+  "Ц",
+  "Ч",
+  "Ш",
+  "Щ",
+  "Ъ",
+  "Ы",
+  "Ь",
+  "Э",
+  "Ю",
+  "Я"
+];
